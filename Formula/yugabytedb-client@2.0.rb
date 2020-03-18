fbf9b960a8637a5bf5d001c7fe195f6e744d2b80
Bhavin Gandhi <bhavin7392@gmail.com>
2020-03-18 12:00:05 +0530

    Have 2.0 as a separate version
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

---
+++ Formula/yugabytedb-client@2.0.rb
@@ -0,0 +1,19 @@
+class YugabytedbClientAT20 < Formula
+  desc "High-performance distributed SQL database Yugabyte DB"
+  homepage "https://yugabyte.com"
+  url "https://downloads.yugabyte.com/yugabyte-2.0.11.0-darwin.tar.gz"
+  sha256 "39acba6800f9d4c665ed44323502cbb650d25430716cd0ecef7135b482d2ce7d" 
+  
+  depends_on :java => "1.8"
+  depends_on "python"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"postgres/bin/ysqlsh"
+    bin.install_symlink libexec/"bin/cqlsh"
+  end
+  
+  test do
+    system "#{libexec}/bin/ysqlsh", "--help"
+  end
+end
--- Formula/yugabytedb@2.0.rb
+++ Formula/yugabytedb@2.0.rb
@@ -1,8 +1,8 @@
-class Yugabytedb < Formula
+class YugabytedbAT20 < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.1.1.0-darwin.tar.gz"
-  sha256 "e9964b97f09bda1a2d2ca1642810ef990c805374a8e1950aad976ff058f16f94"
+  url "https://downloads.yugabyte.com/yugabyte-2.0.11.0-darwin.tar.gz"
+  sha256 "39acba6800f9d4c665ed44323502cbb650d25430716cd0ecef7135b482d2ce7d"
 
   depends_on :java => "1.8"
   depends_on "python"
