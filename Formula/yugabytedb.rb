5504e89521ad58acc38799e0e065d2e1d21fe38d
Bhavin Gandhi <bhavin7392@gmail.com>
2020-04-21 19:26:35 +0530

    test
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- Formula/yugabytedb.rb
+++ Formula/yugabytedb.rb
@@ -1,7 +1,7 @@
 class Yugabytedb < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.1.0.0-darwin.tar.gz"
+  url "https://downloads.yugabyte.com/yugabyte-2.0.11.0-darwin.tar.gz"
   sha256 "cb451a47cc8a3804ed0152f4471b4c3626593da7b401d7bfbd843805beaa10f9"
 
   depends_on :java => "1.8"
